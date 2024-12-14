bcd3e51578ce1df2a1f3f296e22e2ca197b1e8ae
rehanulhb <rehanulh@gmail.com>
2024-12-14 02:49:39 +0100

    Left Nav bar Design and Show Categories from Categories.json Data file

--- src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/Pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 
 const LeftSideNav = () => {
+
+    const [categories, setCategories] = useState([]);
+
+    useEffect(() => {
+        fetch('../data/categories.json')
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
+
     return (
-        <div>
-            <h2 className="text-2xl">Left Side</h2>
+        <div className="space-y-6">
+            <h2 className="text-2xl">All Categories </h2>
+            {
+                categories.map(category => <Link to={`/categor/${category.id}`} className="block ml-4 text-xl font-semibold" key={category.id}>{category.name}</Link>)
+            }
         </div>
     );
 };
